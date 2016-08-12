7b5811a4ae77c8a9ab51e52114428c2c440dd801
George Hilliard <thirtythreeforty@gmail.com>
2016-08-12 00:01:01 -0500

    Mathematica: PlusMinus[] didn't ever work very well, remove it

--- Mathematica/Kernel/init.m
+++ Mathematica/Kernel/init.m
@@ -4,35 +4,6 @@
 
 BeginPackage["thirtythreeforty`"]
 
-(* PlusMinus does not have an intrinsic definition and must be bound.  Similiar
-   rules are recommended by the Wolfram manual, but I grabbed these in particular
-   from `https://github.com/Tyilo/Mathematica-init.m`. *)
-PlusMinus[{a1_, a2_}] := Module[{r},
-	r = {a1, -a2};
-	If[r[[1]] == r[[2]], r[[1]], r, r]
-];
-PlusMinus[a_] := PlusMinus[{a, a}];
-PlusMinus[{a1_, a2_}, {b1_, b2_}] := Module[{r},
-	r = {a1 + b1, a2 - b2};
-	If[r[[1]] == r[[2]], r[[1]], r, r]
-];
-PlusMinus[{a1_, a2_}, b_] := PlusMinus[{a1, a2}, {b, b}];
-PlusMinus[a_, {b1_, b2_}] := PlusMinus[{a, a}, {b1, b2}];
-PlusMinus[a_, b_] := PlusMinus[{a, a}, {b, b}];
-
-MinusPlus[{a1_, a2_}] := Module[{r},
-	r = {-a1, a2};
-	If[r[[1]] == r[[2]], r[[1]], r, r]
-];
-MinusPlus[a_] := MinusPlus[{a, a}];
-MinusPlus[{a1_, a2_}, {b1_, b2_}] := Module[{r},
-	r = {a1 - b1, a2 + b2};
-	If[r[[1]] == r[[2]], r[[1]], r, r]
-];
-MinusPlus[{a1_, a2_}, b_] := MinusPlus[{a1, a2}, {b, b}];
-MinusPlus[a_, {b1_, b2_}] := MinusPlus[{a, a}, {b1, b2}];
-MinusPlus[a_, b_] := MinusPlus[{a, a}, {b, b}];
-
 (* Annotate and display a Table as a LabeledGrid *)
 LabeledGrid[table_, {x_, y_},
    opts : OptionsPattern[{LabeledGrid, Grid}]] :=
