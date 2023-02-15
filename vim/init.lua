bd4608131724866e584486c12e6ca75891881867
George Hilliard <thirtythreeforty@gmail.com>
2023-02-14 23:39:42 -0600

    Update Lessspace, switch to init.lua

---
+++ vim/init.lua
@@ -0,0 +1,21 @@
+vim.cmd([[
+  " Escape should exit terminal insert mode
+  :tnoremap <Esc><Esc> <C-\><C-n>
+
+  " Vterm opens a new vertically-split terminal
+  command! Vterm vsplit | terminal
+  command! Sterm split | terminal
+
+  " And all the regular vim things
+  if filereadable(glob("~/.vimrc"))
+  	source ~/.vimrc
+  endif
+]])
+
+require('telescope').setup {
+  pickers = {
+    git_files = {
+      recurse_submodules = true,
+    },
+  },
+}
